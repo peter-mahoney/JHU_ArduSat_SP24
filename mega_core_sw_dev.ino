cd5ada63f0f7f79846e57eedbb8879e44c275204
peter-mahoney <39682607+peter-mahoney@users.noreply.github.com>
2024-02-29 22:28:17 -0800

    Update mega_core_sw_dev.ino
    updates to get commanding working
    plus adding better comments and TODOs

--- mega_core_sw_dev.ino
+++ mega_core_sw_dev.ino
@@ -1,31 +1,44 @@
 // Code skeleton for ArduSat Arduino Mega Core SW
 // Peter Mahoney SP '24
+
+// Define protocol characters
 #define START_MARKER '<'
 #define END_MARKER   '>'
 #define ADDR_TCS 'A'
+#define ADDR_MEGA 'D'
+#define TLM 'T'
+#define COMMAND 'C'
 
+// Define set of known commands
+char tcsCommandKeys[] = {'A','B','C','D'};
+
+// Setup timebased scheduler
 unsigned long previousMillis = 0;
 unsigned long previousMillisTCS = 0;
-const long interval = 10000; // 10 second interval
+const long tenSec = 10000; // 10 second interval
+const long fiveSec = 5000; // 5 second interval
 
 void setup() {
   // initiate serial interfaces
+  // Serial 0 interface to Serial Monitor over USB
   Serial.begin(9600);
   while (!Serial) {
     ; // Wait for Serial Monitor to open
   }
   // Serial 1 is ArduSat UART bus
   Serial1.begin(9600);
-  // instantiate global variables
+  // Serial 2 interface could be for TTC if desired
 
 }
 
 void loop() {
-  //  Serial.println("Hello from Arduino Mega!");
+  // Adding small delays is required at this stage to slow down the loop
+  // otherwise, the UART buffer can't be read effectively
   delay(100);
   // Send dummy TCS commands
   sendTcsCommands();
-  // Get Telemetry Over UART
+  // Get Telemetry Over UART from subsystems
+  // Simply print to Serial monitor for now
   if (Serial1.available()>0) {
     char receivedChar = Serial1.read();
     if (receivedChar == START_MARKER) {
@@ -42,20 +55,20 @@ void loop() {
             else if (message == END_MARKER) {
               Serial.print("\nEnd of packet.\n");
             }
-            else { Serial.print(message);}
+            else { Serial.print(message);} // Should be "Send to TTC"
           }
       }
     }
   }
   }
-
-  // Print to the Serial terminal every 10 seconds
+  // Dummy mega service loop activity to demonstrate task scheduling
+  // print to the Serial terminal every 10 seconds
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= tenSec) {
     Serial.println("Example Mega Action.");
     previousMillis = currentMillis; // Reset the timer
   }
-
+// To be developed skeleton
     // Process received data from the other board
   // Process and send commands from TTC
     // Send ADCS commands
@@ -75,33 +88,32 @@ void loop() {
   // Package telem and send to TTC over UART
   // EPS housekeeping?
 }
-
+// Should be generalized for multiple subsystems, TCS for now
 void sendTcsCommands(){
   // Send dummy command to enable heater every 5 sec
   bool sendCommand = false;
   char command;
   unsigned long currentMillis = millis();
   unsigned long elapsedMillis = currentMillis - previousMillisTCS;
-  if (elapsedMillis > interval) {
-    command = "A";
+  if (elapsedMillis > fiveSec) {
+    command = 'B';
     sendCommand = true;
+    Serial.println("Mega turning heaters OFF.\n");
     previousMillisTCS = currentMillis; // Reset the timer
   }
-  else if (elapsedMillis < interval && elapsedMillis > interval/2) {
-    command = "B";
-    sendCommand = true;
-    previousMillisTCS = currentMillis; // Reset the timer
-  }
+  // Build and send command using protocol
+  // Pick arbitrary command, will refine this
   if (sendCommand){
     Serial1.print(START_MARKER);
     Serial1.print(ADDR_TCS);
-    Serial1.print(command);
+    Serial1.print(COMMAND);
+    Serial1.print(tcsCommandKeys[1]);
     Serial1.print(END_MARKER);
   }
   }
 void getEpsTelem() {
-  // Gather telem from EPS system
+  // TODO, gather telem from EPS system
 }
 void sendEpsCommands(){
-  // Send commands to EPS system
+  // TODO, send commands to EPS system
 }
