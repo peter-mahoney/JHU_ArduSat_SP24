76b8bb91ae89070b01d214f5059160b8b5654972
peter-mahoney <39682607+peter-mahoney@users.noreply.github.com>
2024-03-03 21:28:48 -0800

    Update uno_tcs_sw_dev.ino
    Updates!

--- uno_tcs_sw_dev.ino
+++ uno_tcs_sw_dev.ino
@@ -2,66 +2,66 @@
 // Peter Mahoney SP '24
 #include <stdio.h>
 #include <string.h>
-// Define UART message protocol
-#define START_MARKER '<'
-#define END_MARKER   '>'
-#define ADDR_TCS 'A'
-#define ADDR_MEGA 'D'
-#define TLM 'T'
-#define COMMAND 'C'
+#include "config.h"
+#define ADDR_TCS "TCS_ADDRS"
+char tcsAddress;
 unsigned long previousMillis = 0;
 const long intervalShort = 2000; // 2 second interval
 const long intervalLong = 5000; // 5 second interval
 bool heaterEnabled = true; 
-
-// Define set of known commands
-char commandKeys[] = {'A','B','C','D'};
+bool heaterOn;
+float temp1; 
+float temp2;
+float temp3;
 
 void setup() {
   // initiate serial interface to main ArduSat bus 
   // also can be serial monitor for debug
   Serial.begin(9600);
   // initiate I2C interfaces if any
-  // instantiate global variables if any
+  // pull in TCS address
+  for (int i = 0; i < sizeof(addrs) / sizeof(addrs[0]); i++) {
+        if (addrs[i].name == ADDR_TCS) {
+            tcsAddress=addrs[i].address;
+            break;
+    }
+  }
 }
 
 void loop() {
   // slow down loop
   delay(100);
+  char commandRead = readCommand();
+  // ! means it wasn't a command received
+  if (commandRead != '!') {
+    processCommand(commandRead);
+  }
+  // Do TCS actions
+  // Process sensor data
+  collectData();
+  // Adjust heater states
   // Send unsolicited telem to mega every 2 seconds
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= intervalShort) {
     sendTlm();
     previousMillis=currentMillis;
   }
-  char commandRead = readCommand();
-  // Z means it wasn't a command received
-  if (commandRead != 'Z') {
-    processCommand(commandRead);
-  }
-  // Process sensor data
-  // Adjust heater states
-  // Check for mega commands
-    // if mega serial available:
-      // read in command
-      // if addressed to TCS:
-         // process command
-            // Turn off heaters
-            // send telem, etc
 }
-void sendTlm() {
-  // Gather telem from system and send
-  float temp1 = 10.00;
-  float temp2 = 25.00;
-  float temp3 = 30.00;
-  bool heaterOn;
+void collectData(){
+  temp1 = 10.00;
+  temp2 = 25.00;
+  temp3 = 30.00;
   if (heaterEnabled == true) {
     heaterOn = true;
   }
   else {
     heaterOn = false;
   }
+}
+void sendTlm() {
+  // Gather telem from system and send
   int heaterOnInt = heaterOn ? 1 : 0;  // Convert bool to int (1 for true, 0 for false)
+  int heaterEnabledInt = heaterEnabled ? 1 : 0; 
   char temp1str[8];
   dtostrf(temp1,5,2,temp1str);
   char temp2str[8];
@@ -71,29 +71,30 @@ void sendTlm() {
   // Buffer to hold the CSV string (adjust the size as needed)
   char csvTcsPacket[50];
   // Format the data into the CSV string
-  snprintf(csvTcsPacket, sizeof(csvTcsPacket), "%s,%s,%s,%d", temp1str, temp2str, temp3str, heaterOnInt);
+  snprintf(csvTcsPacket, sizeof(csvTcsPacket), "%s,%s,%s,%d,%d", temp1str, temp2str, temp3str, heaterEnabledInt, heaterOnInt);
   // Print temperature values for debugging
   Serial.print(START_MARKER);
-  Serial.print(ADDR_TCS);
-  Serial.print(TLM);
+  Serial.print(tcsAddress);
+  Serial.print(TLM_TYPE);
   Serial.print(csvTcsPacket);
   Serial.print(END_MARKER);
+  // TODO, remove this once real TCS code implemented
   heaterEnabled = true;
 }
 // Reads commands in (if any) from UART buffer
 // Should generalize this to other potential
 // UART traffic perhaps
 char readCommand() {
-    char noCommand = 'Z';
+    char noCommand = '!';
     if (Serial.available()>0) {
       char receivedChar = Serial.read();
       if (receivedChar == START_MARKER) {
         char address = Serial.read();
-        if (address == ADDR_TCS) {
+        if (address == tcsAddress) {
           char messageType = Serial.read();
-          if (messageType == COMMAND){
-            char command  = Serial.read();
-            return command;
+          if (messageType == CMD_TYPE){
+            char commandId  = Serial.read();
+            return commandId;
           }
         }
       }
@@ -102,25 +103,28 @@ char readCommand() {
   }
  // Takes in a command character and validates
  // then executes
-void processCommand(char command) {
+void processCommand(char commandId) {
   bool commandConfirmed = false;
-  for (int i = 0; i < sizeof(commandKeys); i++) {
-        if (commandKeys[i] == command) {
+  const char* commandName;
+  for (int i = 0; i < sizeof(tcsCmds)/ sizeof(tcsCmds[0]); i++) {
+        if (tcsCmds[i].id == commandId) {
             commandConfirmed = true;
+            commandName = tcsCmds[i].name;
             break; // Character found in the list
         }
     }
   if (!commandConfirmed){
     return; // Character not found in the list
   }
-  // TODO use case statements here
-  // For now, A is enable heaters
-  if (command == 'A'){
-    heaterEnabled = true;
-  }
-  // B is disable heaters
-  else if (command == 'B'){
-    heaterEnabled = false;
+  switch (commandId) {
+    case 'A':
+      heaterEnabled = true;
+      break;
+    case 'B':
+      heaterEnabled = false;
+      break;
+    default:
+      break;
   }
   // can add C and D eventually
   }
