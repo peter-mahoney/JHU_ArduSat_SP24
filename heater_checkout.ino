9ce6afa91393d3ff3a874dca4dbdd6a3f2307d1e
peter-mahoney <39682607+peter-mahoney@users.noreply.github.com>
2024-03-17 00:24:55 -0700

    Create heater_checkout.ino

---
+++ heater_checkout.ino
@@ -0,0 +1,61 @@
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+
+int transistorPin = 3;
+int tempPin = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  if (! ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+  Serial.println("Measuring voltage and current with INA219 ...");
+  pinMode(transistorPin, OUTPUT); // Set transistorPin as an output
+}
+void loop()
+{
+  int tempReading = analogRead(tempPin);
+  // This is OK
+  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
+  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
+  float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+  // Display Temperature in F
+  Serial.print("Temperature(F): ");Serial.println(tempF);
+  
+  if (tempF < 73.5) {
+    // If the temperature is below 80 degrees Fahrenheit, set the transistor pin high
+    digitalWrite(transistorPin, HIGH);
+  } else if (tempF > 76.0) {
+    // If the temperature is 80 degrees Fahrenheit or above, set the transistor pin low
+    digitalWrite(transistorPin, LOW);
+  }
+
+
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("");
+  delay(500);
+}
