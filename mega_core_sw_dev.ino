9d7a14f457c2e126db95ff890588f4435592b14a
peter-mahoney <39682607+peter-mahoney@users.noreply.github.com>
2024-04-22 22:08:52 -0700

    Update mega_core_sw_dev.ino

--- mega_core_sw_dev.ino
+++ mega_core_sw_dev.ino
@@ -4,13 +4,15 @@
 #include <string.h>
 #include <Adafruit_INA219.h>
 #include "config.h"
-#define ADDR_MEGA "MEGA_ADDRS"
+#define ADDR_CDH "CDH_ADDRS"
 #define ADDR_EPS "EPS_ADDRS"
 #define ADDR_TTC "TTC_ADDRS"
 #define ADDR_TCS "TCS_ADDRS"
 #define ADDR_ADCS "ADCS_ADDRS"
+// Ground test mode?
+bool groundTestMode = true; 
 // variables to hold UART Address
-char megaAddress;
+char cdhAddress;
 char epsAddress;
 char ttcAddress;
 char tcsAddress;
@@ -34,8 +36,15 @@ const long thirtySec = 30000; // 30 second interval
 const long tenMin = 600000; // 10 min interval
 // EPS telem
 bool batteryEnabled;
-bool railEnabled;
-// EPS switches
+bool railEnabled; 
+float busVoltage; //V
+float busCurrent; // mA
+float busPower; // mW
+// float battSoc; //%
+// EPS constants
+const float busVoltageHigh = 7.0; //volts
+const float busVoltageLow = 4.0; //volts
+const float busCurrentHigh = 5000; //mA
 const int batterySwitchPin = 50;
 const int railSwitchPin = 52;
 // Rail voltage sensor
@@ -43,7 +52,7 @@ Adafruit_INA219 ina219;
 
 void setup() {
   // initiate serial interfaces
-  // Serial 0 interface to Serial Monitor over USB
+  // Serial 0 interface to Serial Monitor over USB for testing
   Serial.begin(9600);
   while (!Serial) {
     ; // Wait for Serial Monitor to open
@@ -56,8 +65,8 @@ void setup() {
   ADCS_UART->begin(9600);
   // pull in message addresses
   for (int i = 0; i < sizeof(addrs) / sizeof(addrs[0]); i++) {
-        if (addrs[i].name == ADDR_MEGA) {
-            megaAddress = addrs[i].address;
+        if (addrs[i].name == ADDR_CDH) {
+            cdhAddress = addrs[i].address;
         }
         else if (addrs[i].name == ADDR_EPS) {
             epsAddress = addrs[i].address;
@@ -75,12 +84,12 @@ void setup() {
   // initiate I2C interfaces
   // Current voltage monitor
   if (! ina219.begin()) {
-    Serial.print(START_MARKER);
-    Serial.print(megaAddress);
-    Serial.print(LOG_TYPE);
-    Serial.print('Failed to find INA219 chip');
-    Serial.print(END_MARKER);
-    while (1) { delay(10); }
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(epsAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("Failed to find INA219 chip.");
+    TTC_UART->print(END_MARKER);
+    delay(tenSec);
   }
   // initialize timers/watchdogs
   unsigned long currentMillis = millis();
@@ -100,9 +109,9 @@ void setup() {
   // Initialize EPS switches
   pinMode(batterySwitchPin, OUTPUT);
   pinMode(railSwitchPin, OUTPUT);
-  // EPS enabled at startup
-  batteryEnabled = true;
-  railEnabled = true;
+  // EPS disabled at startup (RECONSIDER)
+  batteryEnabled = false;
+  railEnabled = false;
 }
 
 void loop() {
@@ -116,22 +125,45 @@ void loop() {
   // Get Telemetry or Log over UART from subsystems
   processMessage(*TCS_UART);
   processMessage(*ADCS_UART);
-  // Dummy mega service loop activity to demonstrate task scheduling
-  unsigned long currentMillis = millis();
   // Check watchdogs on each node
+  checkWatchdogs();
+}
+void checkWatchdogs(){
+  // Pet watchdogs (testing only)
+  if (groundTestMode){
+    petWatchdogs(true,true,true);
+  }
+  unsigned long currentMillis = millis();
   if (currentMillis - tcsWatchdog >= thirtySec)
   {
     resetBoard(tcsResetPin);
+    tcsWatchdog = currentMillis;
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(cdhAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("TCS watchdog expired, board reset.");
+    TTC_UART->print(END_MARKER);
   }
   if (currentMillis - adcsWatchdog >= thirtySec)
   {
     resetBoard(adcsResetPin);
+    adcsWatchdog = currentMillis;
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(cdhAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("ADCS watchdog expired, board reset.");
+    TTC_UART->print(END_MARKER);
   }
   if (currentMillis - ttcWatchdog >= tenMin)
   {
     resetBoard(ttcResetPin);
+    ttcWatchdog = currentMillis;
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(cdhAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("TTC watchdog expired, board reset.");
+    TTC_UART->print(END_MARKER);
   }
-  
 }
 void processGroundCommand() {
     char badCommand = '!';
@@ -157,7 +189,7 @@ void processGroundCommand() {
            return; 
         }
         else {commandId  = TTC_UART->read();}
-        if (address == megaAddress){
+        if (address == cdhAddress){
           processMegaCommand(commandId);
         }
         else if (address == epsAddress) {
@@ -176,8 +208,6 @@ void processGroundCommand() {
           nodeUart->print(CMD_TYPE);
           nodeUart->print(commandId);
           nodeUart->print(END_MARKER);
-          // TODO remove debug
-          Serial.println("Sent command to node.");
       }
     }
   }
@@ -189,22 +219,51 @@ void processMegaCommand(char commandId) {
   for (int i = 0; i < sizeof(cdhCmds)/ sizeof(cdhCmds[0]); i++) {
         if (cdhCmds[i].id == commandId) {
             commandConfirmed = true;
+            commandName = cdhCmds[i].name;
             break; // Character found in the list
         }
     }
   if (!commandConfirmed){
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(cdhAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("Command not found by CDH.");
+    TTC_UART->print(END_MARKER);
     return; // Character not found in the list
   }
+  unsigned long currentMillis = millis();
   switch (commandId) {
     case 'A':
-      // reset board
+      // reset TCS
+      resetBoard(tcsResetPin);
+      tcsWatchdog = currentMillis;
       break;
     case 'B':
-      // reset board
+      // reset ADCS
+      resetBoard(adcsResetPin);
+      adcsWatchdog = currentMillis;
       break;
+    case 'C':
+      // reset TTC
+      resetBoard(ttcResetPin);
+      ttcWatchdog = currentMillis;
+    case 'D':
+      // reset TTC
+      resetBoard(ttcResetPin);
+      resetBoard(adcsResetPin);
+      resetBoard(tcsResetPin);
+      ttcWatchdog = currentMillis;
+      adcsWatchdog = currentMillis;
+      tcsWatchdog = currentMillis;
     default:
-      break;
+      return;
   }
+  TTC_UART->print(START_MARKER);
+  TTC_UART->print(cdhAddress);
+  TTC_UART->print(LOG_TYPE);
+  TTC_UART->print("CDH processed command: ");
+  TTC_UART->print(commandName);
+  TTC_UART->print(END_MARKER);
 }
 
 // Get telemetry or logs from subsystems
@@ -217,8 +276,8 @@ void processMessage(Stream &nodeUart) {
     // If start marker found, keep reading, else return
     if (receivedChar == START_MARKER) {
       char address = nodeUart.read();
-      // Confirm address is not MEGA
-      if (address != megaAddress) {
+      // Confirm sender is not MEGA
+      if (address != cdhAddress) {
         char infoType = nodeUart.read();
         // Confirm message is TLM or LOG type
         if (infoType == TLM_TYPE || infoType == LOG_TYPE){
@@ -227,16 +286,16 @@ void processMessage(Stream &nodeUart) {
             for (int i = 0; i < sizeof(addrs) / sizeof(addrs[0]); i++) {
               if (addrs[i].address == address) {
                 subsystem = addrs[i].name;
-                // Reset watchdog for subsystem received
+                // Pet watchdog for subsystem received
                 unsigned long currentMillis = millis();
-                if (subsystem == 'TCS_ADDRS'){
-                  tcsWatchdog = currentMillis; // Reset the tcs watchdog
+                if (subsystem == ADDR_TCS){
+                  tcsWatchdog = currentMillis; // Pet the tcs watchdog
                 }
-                else if (subsystem == 'ADCS_ADDRS'){
-                  adcsWatchdog = currentMillis; // Reset the adcs watchdog
+                else if (subsystem == ADDR_ADCS){
+                  adcsWatchdog = currentMillis; // Pet the adcs watchdog
                 }
-                else if (subsystem == 'TTC_ADDRS'){
-                  ttcWatchdog = currentMillis; // Reset the ttc watchdog
+                else if (subsystem == ADDR_TTC){
+                  ttcWatchdog = currentMillis; // Pet the ttc watchdog
                 }
                 break;
               }
@@ -251,20 +310,34 @@ void processMessage(Stream &nodeUart) {
           Serial.println(subsystem);
           // Route message across TTC UART for downlink
           TTC_UART->print(START_MARKER);
-          TTC_UART->print(ttcAddress);
+          TTC_UART->print(address); //sender address
           TTC_UART->print(infoType);
-          // Loop through message and print to TTC/Serial for testing
+          if (groundTestMode && infoType == 'T'){
+            telemPrettyPrinter(nodeUart, subsystem);
+          }
+          else {
+            while (nodeUart.available() > 0) {
+              char character  = nodeUart.read();
+              // Print character over TTC UART
+              TTC_UART->print(character);
+            }
+          }
+        }
+      }
+     }
+  }
+ }
+void telemPrettyPrinter(Stream &nodeUart, char *subsystem){
+  // Loop through message and print to TTC/Serial for testing
           int id = 0;
           int i=0;
-          char message[5];
+          char message[100];
           message[0] = '\0';
           bool newPoint = true;
           while (nodeUart.available() > 0) {
             char character  = nodeUart.read();
-            // Print character over TTC UART
-            TTC_UART->print(character);
             // TODO remove all below this, integration only
-            if (character != ',' && character != END_MARKER && infoType == TLM_TYPE){
+            if (character != ',' && character != END_MARKER){
               if (newPoint==true){
                 i=0;
                 message[0]='\0';
@@ -292,11 +365,7 @@ void processMessage(Stream &nodeUart) {
               }
             }
           }
-        }
-      }
-     }
-  }
- }
+}
 void resetBoard(int resetPin){
   digitalWrite(resetPin, LOW);
   delay(10);
@@ -313,13 +382,55 @@ void epsLoop() {
   }
 }
 void processEpsTelem() {
-  // TODO, gather telem from EPS system
+  //Gather telem from EPS system
+  busVoltage = ina219.getBusVoltage_V();
+  busCurrent = ina219.getCurrent_mA();
+  busPower = ina219.getPower_mW();
+  // battSoc = 100*(battVoltage/3.7);
 }
 void sendEpsTelem() {
-  // TODO, gather telem from EPS system
+  // Send telem to TTC from EPS system
+  int batteryEnabledInt = batteryEnabled ? 1 : 0;  // Convert bool to int (1 for true, 0 for false)
+  int railEnabledInt = railEnabled ? 1 : 0;
+  char busVStr[8];
+  dtostrf(busVoltage,6,2,busVStr);
+  char busCurStr[8];
+  dtostrf(busCurrent,6,2,busCurStr);
+  char busPowStr[8];
+  dtostrf(busPower,6,2,busPowStr);
+  // Buffer to hold the CSV string (adjust the size as needed)
+  char csvEpsPacket[40];
+  // Format the data into the CSV string
+  snprintf(csvEpsPacket, sizeof(csvEpsPacket), "%s,%s,%s,%d,%d", busVStr, busCurStr, busPowStr, batteryEnabledInt, railEnabledInt);
+  // Send data across UART to MEGA
+  TTC_UART->print(START_MARKER);
+  TTC_UART->print(epsAddress);
+  TTC_UART->print(TLM_TYPE);
+  TTC_UART->print(csvEpsPacket);
+  TTC_UART->print(END_MARKER);
 }
 void processEpsAutonomy(){
-  // TODO, send commands to EPS system
+  // Monitor telemetry and respond to out of limits
+  // Bus voltage out of range, isolate battery
+  if ((busVoltage>busVoltageHigh || busVoltage<busVoltageLow)&& batteryEnabled){
+    batteryEnabled=false;
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(epsAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("Bus voltage out of range, isolating battery.");
+    TTC_UART->print(END_MARKER);
+    setEpsSwitch(batterySwitchPin, batteryEnabled);
+  }
+  // Current draw too high, shutdown rail
+  if (busCurrent>busCurrentHigh && railEnabled){
+    railEnabled=false;
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(epsAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("Bus current out of range, disabling rail.");
+    TTC_UART->print(END_MARKER);
+    setEpsSwitch(railSwitchPin, railEnabled);
+  }
 }
 void processEpsCommand(char commandId) {
   bool commandConfirmed = false;
@@ -327,17 +438,17 @@ void processEpsCommand(char commandId) {
   for (int i = 0; i < sizeof(epsCmds)/ sizeof(epsCmds[0]); i++) {
         if (epsCmds[i].id == commandId) {
             commandConfirmed = true;
-            commandName = tcsCmds[i].name;
+            commandName = epsCmds[i].name;
             break; // Character found in the list
         }
     }
   // Character not found in the list
   if (!commandConfirmed){
-    Serial.print(START_MARKER);
-    Serial.print(epsAddress);
-    Serial.print(LOG_TYPE);
-    Serial.print('Command not found by EPS.');
-    Serial.print(END_MARKER);
+    TTC_UART->print(START_MARKER);
+    TTC_UART->print(epsAddress);
+    TTC_UART->print(LOG_TYPE);
+    TTC_UART->print("Command not found by EPS.");
+    TTC_UART->print(END_MARKER);
     return; 
   }
   // Execute command and send log back to MEGA
@@ -361,12 +472,12 @@ void processEpsCommand(char commandId) {
     default:
       return;
   }
-  Serial.print(START_MARKER);
-  Serial.print(epsAddress);
-  Serial.print(LOG_TYPE);
-  Serial.print('EPS processed command: ');
-  Serial.print(commandName);
-  Serial.print(END_MARKER);
+  TTC_UART->print(START_MARKER);
+  TTC_UART->print(epsAddress);
+  TTC_UART->print(LOG_TYPE);
+  TTC_UART->print("EPS processed command: ");
+  TTC_UART->print(commandName);
+  TTC_UART->print(END_MARKER);
   }
 void setEpsSwitch(int switchPin, bool on){
   if (on==true){
@@ -374,5 +485,17 @@ void setEpsSwitch(int switchPin, bool on){
   }
   else {
     digitalWrite(switchPin,LOW);
+  }
+}
+void petWatchdogs(bool ttc, bool tcs, bool adcs){
+  unsigned long currentMillis = millis();
+  if (ttc==true){
+    ttcWatchdog = currentMillis;
+  }
+  if (tcs==true){
+    tcsWatchdog = currentMillis;
+  }
+  if (adcs==true){
+    adcsWatchdog = currentMillis;
   }
 }
